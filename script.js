c9deaadef7e54c3dfb3219dc62b36e1d5ef675cf
Sam <super8989@gmail.com>
2019-09-15 18:40:53 -0400

    countdown timer setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('.clock');
+
+endDate.addEventListener('change', function(e) {
+    e.preventDefault();
+})
