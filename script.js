3db37cc1ae8091469078dbacbecbf45b12ed1764
Sam <super8989@gmail.com>
2019-09-15 18:45:39 -0400

    getting input time for the countdown

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ const clock = document.querySelector('.clock');
 
 endDate.addEventListener('change', function(e) {
     e.preventDefault();
+    console.log(endDate.value);
+
+    const temp = new Date(endDate.value);
+    console.log(temp);
+
+    startClock(temp);
 })
+
+function startClock(d) {
+    console.log(d);
+}
+
+function timeLeft(d) {
+    
+}
+
