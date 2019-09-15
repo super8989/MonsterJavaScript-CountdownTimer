c909becda4373e26f6335a2762b19ddd7e5eaaa4
Sam <super8989@gmail.com>
2019-09-15 19:34:29 -0400

    calculating time left

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const endDate = document.querySelector('input[name="endDate"]');
 const clock = document.querySelector('.clock');
 
-endDate.addEventListener('change', function(e) {
+endDate.addEventListener('change', function (e) {
     e.preventDefault();
+
     console.log(endDate.value);
 
     const temp = new Date(endDate.value);
@@ -12,10 +13,29 @@ endDate.addEventListener('change', function(e) {
 })
 
 function startClock(d) {
-    console.log(d);
+    console.log(timeLeft(d));
 }
 
 function timeLeft(d) {
-    
-}
+    let currentDate = new Date();
+    // console.log(Date.parse(d));
+    // console.log(currentDate);
+    // console.log(Date.parse(currentDate));
 
+    let t = Date.parse(d) - Date.parse(currentDate);
+    // console.log(t);
+
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    let days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+
+    };
+}
